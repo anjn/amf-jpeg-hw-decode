2e3506162c72b4032372451de271be3b0f6ab768
Jun Ando <jun.ando@amd.com>
2025-03-25 09:30:25 +0900

    Update code.

--- test.cpp
+++ test.cpp
@@ -8,8 +8,6 @@
 #include "public/include/components/VideoDecoderUVD.h" // AMFVideoDecoderUVD_MJPEG
 #include "public/common/AMFFactory.h"
 
-#include "utils.hpp"
-
 // https://github.com/corkami/formats/blob/master/image/jpeg.md
 
 struct jpeg_segment {
@@ -108,6 +106,30 @@ jpeg_image parse_jpeg(const std::vector<unsigned char>& data)
     printf("Image size %d x %d\n", image.width, image.height);
 
     return image;
+}
+
+void print(amf::AMF_ACCELERATION_TYPE accel_type)
+{
+    switch(accel_type) {
+    case amf::AMF_ACCEL_NOT_SUPPORTED: printf("AMF_ACCEL_NOT_SUPPORTED\n"); break;
+    case amf::AMF_ACCEL_HARDWARE: printf("AMF_ACCEL_HARDWARE\n"); break;
+    case amf::AMF_ACCEL_GPU: printf("AMF_ACCEL_GPU\n"); break;
+    case amf::AMF_ACCEL_SOFTWARE: printf("AMF_ACCEL_SOFTWARE\n"); break;
+    default: printf("%d\n", accel_type);
+    }
+}
+
+void print(AMF_RESULT result)
+{
+    switch (result) {
+        case AMF_OK: printf("AMF_OK\n"); break;
+        case AMF_FAIL: printf("AMF_FAIL\n"); break;
+        case AMF_EOF: printf("AMF_EOF\n"); break;
+        case AMF_INPUT_FULL: printf("AMF_INPUT_FULL\n"); break;
+        case AMF_DECODER_NO_FREE_SURFACES: printf("AMF_DECODER_NO_FREE_SURFACES\n"); break;
+        case AMF_REPEAT: printf("AMF_REPEAT\n"); break;
+        default: printf("%d\n", result);
+    }
 }
 
 void copy_plane(amf::AMFPlanePtr plane, uint8_t* dst)
